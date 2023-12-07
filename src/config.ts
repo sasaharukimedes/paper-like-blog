3627c3b85df455ac8491f05776ef1f8cb1a915d0
sasaharukimedes <sasaharu3@gmail.com>
2023-12-08 05:59:01 +0900

    add site weburl

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://my-updating-blogs.pages.dev/", // replace this with your deployed domain
   author: "Harukimedes",
   desc: "This is Harukimedes's Blog",
   title: "My Updating (B)logs",
