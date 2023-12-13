df3e67c4ef268849cddabd3a36b5811d8e99491b
sasaharukimedes <sasaharu3@gmail.com>
2023-12-13 19:15:46 +0900

    rewrite search sentence

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -82,11 +82,11 @@ export default function SearchBar({ searchList }: Props) {
           </svg>
         </span>
         <input
-          className="block w-full rounded border border-skin-fill 
+          className="block w-full rounded border border-skin-fill
         border-opacity-40 bg-skin-fill py-3 pl-10
-        pr-3 placeholder:italic placeholder:text-opacity-75 
+        pr-3 placeholder:italic placeholder:text-opacity-75
         focus:border-skin-accent focus:outline-none"
-          placeholder="Search for anything..."
+          placeholder="記事のタイトルや内容を思い出して書いてください！"
           type="text"
           name="search"
           value={inputVal}
